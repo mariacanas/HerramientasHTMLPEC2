2f71165ab3b7b968b6923159845c529081c5c042
mariacanas <maryber221@gmail.com>
2024-05-26 20:36:18 +0200

    Codigo PEC2

---
+++ dist/index.09c24910.js
@@ -0,0 +1,7 @@
+function responsiveMenu() {
+    var x = document.getElementById("nav");
+    if (x.className === "") x.className = "responsive";
+    else x.className = "";
+}
+
+//# sourceMappingURL=index.09c24910.js.map
