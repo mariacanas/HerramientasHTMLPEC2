466a6d9271404287853bb3adc12bdc121b74a2ba
mariacanas <maryber221@gmail.com>
2024-06-17 22:38:04 +0200

    Update js

--- src/js/script.js
+++ src/js/script.js
@@ -7,3 +7,23 @@ function responsiveMenu(){
         x.className = "";
     }
 }
+
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    var lazyVideos = [].slice.call(document.querySelectorAll('.youtube-lazyload'));
+
+    lazyVideos.forEach(function(lazyVideo) {
+        var playButton = lazyVideo.querySelector('.play-button');
+        playButton.addEventListener('click', function() {
+            var iframe = document.createElement('iframe');
+            iframe.src = 'https://www.youtube.com/embed/' + lazyVideo.dataset.embed + '?autoplay=1';
+            iframe.width = '560';
+            iframe.height = '315';
+            iframe.allow = 'accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share';
+            iframe.allowFullscreen = true;
+            lazyVideo.parentNode.replaceChild(iframe, lazyVideo);
+        });
+    });
+});
+
